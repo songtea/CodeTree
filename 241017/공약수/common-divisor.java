3c19322b06ca5a26c01ebc59945b821eb7dd5536
songtea <85604555+songtea@users.noreply.github.com>
2024-10-17 00:49:55 +0900

    Title: 공약수, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241017/공약수/common-divisor.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] b = new int[sc.nextInt()];
+
+        for(int i = 0; i < b.length; i++) {
+            b[i] = sc.nextInt();
+        }
+
+    }
+}
