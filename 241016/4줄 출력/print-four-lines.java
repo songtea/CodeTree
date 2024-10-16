36c4066397efaf6c2c29a28836480387daf0405d
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 09:17:54 +0900

    Title: 4줄 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241016/4줄 출력/print-four-lines.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Height");
+        System.out.println(200 + " Cm");
+        System.out.println("Weight");
+        System.out.println(138.723 + " Kg");
+    }
+}
