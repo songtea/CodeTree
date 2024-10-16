dfdd2bf2ef7a6fde38f38db5052a6d8230ff9d0d
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 09:08:43 +0900

    Title: 2줄 출력 2, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241016/2줄 출력 2/print-two-lines-2.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Do you know Kimchi?");
+        System.out.println("My Favorite Korean Food Is Kimchi!");
+    }
+}
