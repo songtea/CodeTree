fad21cce4d47ec19b0ed048c806afac1866b7511
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 16:41:16 +0900

    Title: 단위 변환하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241016/단위 변환하기/change-unit.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double mile = 160934.40;
+        double fit = 30.48;
+
+        System.out.print("2.80 mi = ");
+        System.out.printf("%.2f", 2.80 * mile);
+        System.out.println("");
+        System.out.print("128.40 ft = ");
+        System.out.printf("%.2f", 128.40 * fit);
+    }
+}
