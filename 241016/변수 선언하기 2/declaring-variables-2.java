4b37dd4769a5603836576ae741e46a0ab60cf915
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 16:16:31 +0900

    Title: 변수 선언하기 2, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241016/변수 선언하기 2/declaring-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 365;
+        Character y = 'Y';
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
