63276a3c1c25e61669056d32f95fc0bf69f3b011
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 09:19:07 +0900

    Title: 공백 넣어서 출력, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241016/공백 넣어서 출력/insert-space-and-print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("score          subject          name");
+        System.out.println(" ");
+        System.out.println("  100             math           sam");
+        System.out.println("   88          science          paul");
+        System.out.println("   96         language          jack");
+    }
+}
