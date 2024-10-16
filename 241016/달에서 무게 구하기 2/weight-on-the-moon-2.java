5054498675820ad109c3a52579f9c6a912c37329
songtea <85604555+songtea@users.noreply.github.com>
2024-10-16 16:36:02 +0900

    Title: 달에서 무게 구하기 2, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241016/달에서 무게 구하기 2/weight-on-the-moon-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 147;
+        double b = 0.16500;
+
+        System.out.print(a + " * ");
+        System.out.printf("%.5f",b);
+        System.out.print(" = ");
+        System.out.printf("%.5f",(a * b));
+    }
+}
