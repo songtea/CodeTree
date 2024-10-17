954a7529adaead8d3a8a4dd4cb9d0612cd901693
songtea <85604555+songtea@users.noreply.github.com>
2024-10-17 22:52:53 +0900

    Title: 실수 받아 그대로 출력, Time: 171ms, Memory: 12MB, Status: Passed - CodeTree

--- 241017/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 241017/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        System.out.println(a + 0.0033);
+        double num = sc.nextDouble();
+        double num2 = Math.round(num*1000)/1000.0;
+
+        System.out.printf("%.2f",num);
+        sc.close();
     }
 }
