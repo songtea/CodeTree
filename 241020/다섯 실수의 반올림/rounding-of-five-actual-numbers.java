7f400b7be19cb22870d3c53aa54132c46aac5e0b
songtea <85604555+songtea@users.noreply.github.com>
2024-10-20 15:12:45 +0900

    Title: 다섯 실수의 반올림, Time: 142ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241020/다섯 실수의 반올림/rounding-of-five-actual-numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+        double e = sc.nextDouble();
+
+        System.out.printf("%.3f", a);
+        System.out.println("");
+        System.out.printf("%.3f", b);
+        System.out.println("");
+        System.out.printf("%.3f", c);
+        System.out.println("");
+        System.out.printf("%.3f", d);
+        System.out.println("");
+        System.out.printf("%.3f", e);
+        System.out.println("");
+    }
+}
