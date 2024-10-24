0af3ebe4dd90cec0cd72146bcb8a02657609c69b
songtea <85604555+songtea@users.noreply.github.com>
2024-10-24 17:39:43 +0900

    Title: 수열 뒤집기, Time: 275ms, Memory: 20MB, Status: Passed - CodeTree

--- 241024/수열 뒤집기/invert-sequence.java
+++ 241024/수열 뒤집기/invert-sequence.java
@@ -43,6 +43,10 @@ public class Main {
             }
         }
 
+        for(int i = 1; i <= arrlength; i++) {
+            arr[i - 1] = i;
+        }
+
         for(int i = 0; i < minRepeat; i++) {
             int[] arratemp = Arrays.copyOfRange(arr, a1 - 1, a2);
             int aidx = 0;
