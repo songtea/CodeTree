e90ad77fd97c558bb409930c70aaf8fca635acf6
songtea <85604555+songtea@users.noreply.github.com>
2024-10-25 23:08:43 +0900

    Title: 실수형 변수 선언하기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241025/실수형 변수 선언하기/declare-real-variable.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 12.3, b = 70.0;
+
+        System.out.printf("%.2f", a);
+        System.out.print("     ");
+        System.out.printf("%.2f", b);
+        System.out.print("     ");
+        System.out.printf("%.2f", a+b);
+    }
+}
