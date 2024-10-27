f57abc524fb8f1354db45c3598314fad6889a7a0
songtea <85604555+songtea@users.noreply.github.com>
2024-10-27 23:50:07 +0900

    Title: 정수와 실수의 곱셈, Time: 125ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241027/정수와 실수의 곱셈/multiplication-of-integers-and-real-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 9;
+        double b = 358.273;
+
+        System.out.print(a + " * ");
+        System.out.printf("%.3f", b);
+        System.out.print(" = ");
+        System.out.printf("%.3f", a * b);
+    }
+}
