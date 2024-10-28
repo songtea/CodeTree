2c987ce63208018d2951a0c4e886c463a71458fa
songtea <85604555+songtea@users.noreply.github.com>
2024-10-28 23:14:10 +0900

    Title: 길이 단위 변환하기 2, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241028/길이 단위 변환하기 2/change-length-unit-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double yd = 91.44;
+        int mi = 160934;
+
+        System.out.print("6.8yd = ");
+        System.out.printf("%.1f", yd * 6.8);
+        System.out.print("cm");
+        System.out.println("");
+        System.out.print("0.4mi = ");
+        System.out.printf("%.1f", mi * 0.4);
+        System.out.print("cm");
+    }
+}
